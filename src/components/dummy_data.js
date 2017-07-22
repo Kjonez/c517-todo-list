208f9f98f3412105a874aab4baa955f4d406a6c9
Scott Ninja Bowler <scott.bowler@learningfuze.com>
2017-07-21 18:12:38 -0700

    Todo in-progress

---
+++ src/components/dummy_data.js
@@ -0,0 +1,17 @@
+export const todo_data = [
+    {
+        title: 'Teach React',
+        description: 'Teach c517 some react stuff',
+        complete: false
+    },
+    {
+        title: 'Feed the bear',
+        description: 'He\'s getting really hungry',
+        complete: false
+    },
+    {
+        title: 'Wash car',
+        description: 'Wash the car and all the things',
+        complete: false
+    }
+];
