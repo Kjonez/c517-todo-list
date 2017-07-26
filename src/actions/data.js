30d34cd8de6d937133bacad9dd69afbd031982b1
Scott Ninja Bowler <scott.bowler@learningfuze.com>
2017-07-26 16:27:25 -0700

    Redux version in-progress

---
+++ src/actions/data.js
@@ -0,0 +1,14 @@
+export default [
+    {
+        title: 'Wash Car',
+        complete: false
+    },
+    {
+        title: 'Clean House',
+        complete: false
+    },
+    {
+        title: 'Feed the bear',
+        complete: false
+    }
+]
---
+++ src/actions/index.js
@@ -0,0 +1,9 @@
+import types from './types';
+import data from './data';
+
+export function getTodos(){
+    return {
+        type: types.GET_LIST,
+        payload: data
+    }
+}
